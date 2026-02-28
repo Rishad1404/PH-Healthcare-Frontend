64b65947df03e9b08543a93c3179cf891ef829b7
Rishad1404 <rishadislam2019@gmail.com>
2026-02-28 21:57:52 +0600

    shadcn components ui added

---
+++ src/app/(dashboardLayout)/(commonProtectedLayout)/my-profile/loading.tsx
@@ -0,0 +1,4 @@
+export default function CommonMyProfileLoading() {
+  // Or a custom loading skeleton component
+  return <p>Loading My Profile...</p>
+}
---
+++ src/app/(dashboardLayout)/admin/dashboard/doctor-specialties-management/loading.tsx
@@ -0,0 +1,4 @@
+export default function AdminsDoctorSpecialtiesManagementLoading() {
+  // Or a custom loading skeleton component
+  return <p>Loading Doctors Specialties Management...</p>
+}
